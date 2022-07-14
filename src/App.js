7573660e833f410ce2f400efc79cbf83d1851391
Najim Rizky <najim.rizky@student.umn.ac.id>
2022-07-14 15:37:10 +0700

    implement router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
 import Quiz from './components/Quiz';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home"
 
 function App() {
     return (
-        <Quiz />
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/quiz" element={<Quiz/>}/>
+        </Routes>
     );
 }
 

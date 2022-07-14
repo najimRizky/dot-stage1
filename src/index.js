773fb17c618fc1bfef4fb80985e68bac7ad9ebc5
Najim Rizky <najim.rizky@student.umn.ac.id>
2022-07-14 16:00:36 +0700

    install & implement redux

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import rootReducer from './reducers/rootReducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(rootReducer)
 root.render(
   //<React.StrictMode>
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
   //</React.StrictMode>
 );
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+const initState = {
+    user: null
+}
+
+const rootReducer = (state = initState, action) => {
+    return state;
+}
+
+export default rootReducer

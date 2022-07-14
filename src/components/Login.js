ddf64f7564936f8dbf9687793eb6098da9850191
Najim Rizky <najim.rizky@student.umn.ac.id>
2022-07-14 18:48:25 +0700

    create login register ui

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,29 @@
+import { Button, Divider, TextField, Typography } from "@mui/material";
+import { Box, Container } from "@mui/system";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-    return (  
-        <h1>Login</h1>
+    const login = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <Container sx={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+            <Box sx={{minWidth: "300px", width: "40vw", bgcolor: "#fbfbfb", p: "20px"}}>
+                <Typography variant={"h2"} fontWeight={"bold"} fontSize={"28px"} textAlign={"center"}>Login</Typography>
+                <form onSubmit={login} className="form">
+                    <TextField required sx={{my: "8px"}} fullWidth type={"email"} label="Email" variant="standard" />
+                    <TextField required sx={{my: "8px"}} fullWidth type={"password"} label="Password" variant="standard" />
+                    <Button sx={{my: "8px"}} fullWidth type={"submit"} variant={"contained"}>Login</Button>
+                    <Divider>Or</Divider>
+                    <Box sx={{textAlign: "center"}} >
+                        <Link to="/register" style={{textDecoration: "none"}}>
+                            <Button link variant={"text"}>Register here</Button>
+                        </Link>
+                    </Box>
+                </form>
+            </Box>
+        </Container>
     );
 }
- 
+
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,30 @@
+import { Button, Divider, TextField, Typography } from "@mui/material";
+import { Box, Container } from "@mui/system";
+import { Link } from "react-router-dom";
+
 const Register = () => {
-    return (  
-        <h1>Register</h1>
+    const register = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <Container sx={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+            <Box sx={{minWidth: "300px", width: "40vw", bgcolor: "#fbfbfb", p: "20px"}}>
+                <Typography variant={"h2"} fontWeight={"bold"} fontSize={"28px"} textAlign={"center"}>Register</Typography>
+                <form onSubmit={register} className="form">
+                    <TextField required sx={{my: "8px"}} fullWidth type={"text"} label="Name" variant="standard" />
+                    <TextField required sx={{my: "8px"}} fullWidth type={"email"} label="Email" variant="standard" />
+                    <TextField required sx={{my: "8px"}} fullWidth type={"password"} label="Password" variant="standard" />
+                    <Button sx={{my: "8px"}} fullWidth type={"submit"} variant={"contained"}>Register</Button>
+                    <Divider>Or</Divider>
+                    <Box sx={{textAlign: "center"}} >
+                        <Link to="/login" style={{textDecoration: "none"}}>
+                            <Button link variant={"text"}>Login here</Button>
+                        </Link>
+                    </Box>
+                </form>
+            </Box>
+        </Container>
     );
 }
- 
+
 export default Register;

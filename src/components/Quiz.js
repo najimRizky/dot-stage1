95f5730beef0c776b93766735ae1df97cbeb6ec7
Najim Rizky <najim.rizky@student.umn.ac.id>
2022-07-14 12:26:16 +0700

    count result

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,4 +1,4 @@
-import { CircularProgress, FormControlLabel, Radio, RadioGroup } from "@mui/material";
+import { CircularProgress, FormControlLabel, Radio, RadioGroup, Button } from "@mui/material";
 import { Box, Container } from "@mui/system";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -6,48 +6,79 @@ import { useEffect, useState } from "react";
 const Quiz = () => {
     const [questions, setQuestion] = useState(null)
     const [answers, setAnswer] = useState({})
+    const [result, setResult] = useState(null)
     const getQuestion = () => {
         axios.get('https://opentdb.com/api.php?amount=10&type=boolean&category=18')
         .then((results) => {
-            // console.log(results.data.results)
+            console.log(results.data.results)
             setQuestion(results.data.results)
         })
         .catch((err) => {
             console.log(err)
         })
     }
+
+    // Run Once on first render
     useEffect(() => {
         getQuestion()
     }, [])
 
     useEffect(() => {
-        console.log(answers)
+        // console.log(answers)
     }, [answers])
-
 
     const handleChange = (i, value) => {
         setAnswer({...answers, [i]: value})
     }
+
+    const checkAnswer = () => {
+        let resultTmp = {
+            correct: 0,
+            wrong: 0,
+            details: {}
+        }
+        questions.forEach((question, i) => {
+            if(answers[i] === question.correct_answer){
+                resultTmp.details[i] = true
+                resultTmp["correct"]++
+            }else{
+                resultTmp.details[i] = false
+                resultTmp["wrong"]++
+            }
+        })
+        setResult(resultTmp)
+        // console.log(resultTmp)
+    }
+
     return (
         <Container>
             <h1>Quiz</h1>
                 {questions === null ? (<Box sx={{display: "flex", justifyContent: "center"}}><CircularProgress/></Box>) : (
-                    <ol>
-                        {questions.map((question, i) => 
-                            <li key={i}>
-                                <p dangerouslySetInnerHTML={{ __html: question.question }} />
-                                <RadioGroup
-                                    aria-labelledby="demo-radio-buttons-group-label"
-                                    defaultValue=""
-                                    name={`question ${i}`}
-                                    onChange={(e) => handleChange(i, e.target.value) }
-                                >   
-                                    <FormControlLabel value="true" control={<Radio />} label="True" />
-                                    <FormControlLabel value="false" control={<Radio />} label="False" />
-                                </RadioGroup>
-                            </li>
+                    <>
+                        <ol>
+                            {questions.map((question, i) => 
+                                <li key={i}>
+                                    <p dangerouslySetInnerHTML={{ __html: question.question }} />
+                                    <RadioGroup
+                                        aria-labelledby="demo-radio-buttons-group-label"
+                                        defaultValue=""
+                                        name={`question ${i}`}
+                                        onChange={(e) => handleChange(i, e.target.value)}
+                                    >   
+                                        <FormControlLabel value="True" control={<Radio />} label="True" />
+                                        <FormControlLabel value="False" control={<Radio />} label="False" />
+                                    </RadioGroup>
+                                </li>
+                            )}
+                        </ol>
+                        {result !== null && (
+                            <>
+                                <p>Correct Answers: {result.correct}</p>
+                                <p>Wrong Answers: {result.wrong}</p>
+                            </>
                         )}
-                    </ol>
+                        <Button variant="contained" onClick={checkAnswer}>Submit</Button>
+                    </>
                 )}
         </Container>
     )
